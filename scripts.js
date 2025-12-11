77461921003fe86ed28030d85593857fce8a3a30
marchenkov.gleb <g3niusS.s@yandex.ru>
2025-12-11 17:08:12 +0300

    feat: логика отображения текущего цвета

--- scripts.js
+++ scripts.js
@@ -1,9 +1,12 @@
 const redSlider = document.getElementById('red-slider');
 const colorPreview = document.getElementById('color-preview');
+const nowColor = document.getElementById('color-code');
+
 const redValue = document.getElementById('red-value');
 redSlider.addEventListener('input', function(event){
     redValue.textContent = event.target.value;
     colorPreview.style.backgroundColor = `rgb(${redValue.textContent}, ${greenValue.textContent}, ${blueValue.textContent})`;
+    nowColor.textContent = `RGB(${redValue.textContent}, ${greenValue.textContent}, ${blueValue.textContent})`;
 });
 
 const greenSlider = document.getElementById('green-slider');
@@ -11,6 +14,7 @@ const greenValue = document.getElementById('green-value');
 greenSlider.addEventListener('input', function(event){
     greenValue.textContent = event.target.value;
     colorPreview.style.backgroundColor = `rgb(${redValue.textContent}, ${greenValue.textContent}, ${blueValue.textContent})`;
+    nowColor.textContent = `RGB(${redValue.textContent}, ${greenValue.textContent}, ${blueValue.textContent})`;
 });
 
 const blueSlider = document.getElementById('blue-slider');
@@ -18,4 +22,5 @@ const blueValue = document.getElementById('blue-value');
 blueSlider.addEventListener('input', function(event){
     blueValue.textContent = event.target.value;
     colorPreview.style.backgroundColor = `rgb(${redValue.textContent}, ${greenValue.textContent}, ${blueValue.textContent})`;
+    nowColor.textContent = `RGB(${redValue.textContent}, ${greenValue.textContent}, ${blueValue.textContent})`;
 });
